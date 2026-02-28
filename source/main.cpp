7501ed1d99b76f6a0402b0dca4f3faf234805234
Jakub Fratczak <jakubfratczak204@gmail.com>
2026-02-28 23:02:17 +0100

    [FEATURE] make the entry callback functions try-catch functions

--- source/main.cpp
+++ source/main.cpp
@@ -13,29 +13,48 @@
 #include "shader/shader.hpp"
 #include "utility/unique_pointer.hpp"
 
-SDL_AppResult SDL_AppInit(void** const appstate, int const, char** const)
+SDL_AppResult SDL_AppInit(void** const app_state, int const, char** const) try
 {
 	SDL_InitSubSystem(SDL_INIT_VIDEO);
-	*appstate = new fro::Application();
+	*app_state = new fro::Application();
 	return SDL_APP_CONTINUE;
 }
-
-SDL_AppResult SDL_AppIterate(void* const appstate)
+catch (std::exception& exception)
 {
-	static_cast<fro::Application*>(appstate)->tick();
-	return SDL_APP_CONTINUE;
+	SDL_Log(exception.what());
+	return SDL_APP_FAILURE;
 }
 
-SDL_AppResult SDL_AppEvent(void* const appstate, SDL_Event* const event)
+SDL_AppResult SDL_AppIterate(void* const app_state) try
+{
+	static_cast<fro::Application*>(app_state)->tick();
+	return SDL_APP_CONTINUE;
+}
+catch (std::exception& exception)
+{
+	SDL_Log(exception.what());
+	return SDL_APP_FAILURE;
+}
+
+SDL_AppResult SDL_AppEvent(void* const app_state, SDL_Event* const event) try
 {
 	return
-		static_cast<fro::Application*>(appstate)->process_event(*event)
+		static_cast<fro::Application*>(app_state)->process_event(*event)
 			? SDL_APP_CONTINUE
 			: SDL_APP_SUCCESS;
 }
-
-void SDL_AppQuit(void* const appstate, SDL_AppResult const)
+catch (std::exception& exception)
 {
-	delete static_cast<fro::Application*>(appstate);
+	SDL_Log(exception.what());
+	return SDL_APP_FAILURE;
+}
+
+void SDL_AppQuit(void* const app_state, SDL_AppResult const) try
+{
+	delete static_cast<fro::Application*>(app_state);
 	SDL_QuitSubSystem(SDL_INIT_VIDEO);
+}
+catch (std::exception& exception)
+{
+	SDL_Log(exception.what());
 }
